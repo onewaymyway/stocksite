1995999b0c14205b0de29e8f79632c3792dcd8bb
OneWay <839357821@qq.com>
2018-01-05 18:21:34 +0800

    增加排序工具类

--- StockQQServer/plugins/sample.py
+++ StockQQServer/plugins/sample.py
@@ -5,6 +5,41 @@
 # 2. 将本文件保存至 ~/.qqbot-tmp/plugins 目录 （或 c:\user\xxx\.qqbot-tmp\plugins ）
 # 3. 在命令行窗口输入： qq plug sample
 import tushare as ts
+import os
+import shutil
+import sys
+import json
+import valueTools as vt
+
+myRoot=os.getcwd().replace("\\","/")+"/"
+
+configO={}
+myDataO={}
+def initDatas():
+    global configO
+    configO["stockDataPath"]=os.path.normpath(myRoot+"../../StockView/bin/h5/last.json")
+    stockO=readStockData()
+    
+    stockList=stockO["stocks"]
+    typeList=stockO["types"]
+    myDataO["stocks"]=stockList
+    myDataO["types"]=typeList
+    stockDic={}
+    for tStock in stockList:
+        stockDic[tStock["code"]]=tStock
+    myDataO["stockDic"]=stockDic
+def getStockDataOByCode(code):
+    stockDic=myDataO["stockDic"]
+    if code in stockDic:
+        return stockDic[code]
+    return None
+    
+def readStockData():
+    f=open(configO["stockDataPath"],"r",encoding="utf-8")
+    jsStr=f.read()
+    f.close()
+    jsO=json.loads(jsStr)
+    return jsO
 
 def getGoodPercent(tPrice,prePrice):
     return round((tPrice-prePrice)*100/prePrice,2)
@@ -34,5 +69,26 @@ def onQQMessage(bot, contact, member, content):
     if content.find("获取指数")>=0:
         bot.SendTo(contact,getIndexInfo())
         return
-    
+ 
+def getRank(rankID,count):
+    stockO=readStockData()
+    stockList=stockO["stocks"]
+    typeList=stockO["types"]
+    #print(stockList)
+    #print(typeList)
+    t=typeList[rankID]
+    print(t)
+    sortParams=t["sortParams"]
+    s=vt.sortListByKey(stockList,sortParams[0],sortParams[1],sortParams[2])
+    rstList=[t["tip"]]
+    for i in range(0,count):
+        rstList.append(vt.getTplStr(t["tpl"],s[i][t["dataKey"]]))
+    rst="\n\n".join(rstList)
+    print(rst)
+    return rst
 
+def mTest():
+    getRank(25,10)
+    print(getStockDataOByCode("601918"))
+initDatas()
+mTest()
---
+++ StockQQServer/plugins/valueTools.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+tplArrDic={}
+def getFlatKeyValue(obj,key):
+    if not obj:
+        return None
+    if not key:
+        return obj
+    keys=key.split(".")
+    tV=obj
+    try:
+        for tkey in keys:
+            tV=tV[tkey]
+    except:
+        return None
+    return tV
+
+def getFlatKeyValueNum(obj,key):
+    rst=getFlatKeyValue(obj,key)
+    if not rst:
+        return 0
+    return getNum(rst)
+
+def getFlatKeyValueStr(obj,key):
+    rst=getFlatKeyValue(obj,key)
+    if not rst:
+        return ""
+    return getStr(rst)
+
+def getNum(v):
+    #print(v,type(v))
+    if type(v)!="float":
+        return float(v)
+    return v;
+
+def getStr(v):
+    if type(v)!="str":
+        return str(v)
+    return v
+
+def sortBigFirst(a, b):
+    a=getStr(a)
+    b=getStr(b)
+
+    if a==b:
+        return 0
+    return 1 if b>a else -1
+
+def sortSmallFirst(a, b):
+    a=getStr(a)
+    b=getStr(b)
+
+    if a==b:
+        return 0
+    return -1 if b>a else 1
+
+def sortNumBigFirst(a, b):
+    a=getNum(a)
+    b=getNum(b)
+    if a==b:
+        return 0
+    return 1 if b>a else -1
+
+def sortNumSmallFirst(a, b):
+    a=getNum(a)
+    b=getNum(b)
+    if a==b:
+        return 0
+    return -1 if b>a else 1
+    
+def sortByKey(key,forceNum = True):
+    def msort(a):
+        return getNum(a[key]) if forceNum else getStr(a[key])
+    return msort;
+    
+def sortByKeyEX(key,forceNum = True):
+    if not "." in key:
+        return sortByKey(key,forceNum)
+    
+    getKeyFun=getFlatKeyValueNum if forceNum else getFlatKeyValueStr
+    def msort(a):
+        return getKeyFun(a, key)
+    return msort;
+    
+def sortListByKey(dataList,key,bigFirst=False,forceNum=True):
+    return sorted(dataList,key=sortByKeyEX(key,forceNum),reverse = bigFirst)
+
+def copyArr(arr):
+    return arr[:]
+
+def splitStpl(tplStr):
+    if tplStr in tplArrDic:
+        return copyArr(tplArrDic[tplStr])
+    rst=[]
+    preI=0
+    for i in range(0,len(tplStr)):
+        tStr=tplStr[i]
+        if tStr=="#":
+            if i-1>=preI:
+                #print(preI,i)
+                rst.append(tplStr[preI:i])
+            preI=i+1
+                
+        if tStr=="{" or tStr=="}":
+            if i-1>=preI:
+                rst.append(tplStr[preI:i])
+            rst.append(tStr)
+            preI=i+1
+    if i-1>=preI:
+        rst.append(tplStr[preI:i])
+    tplArrDic[tplStr]=rst
+    return copyArr(rst)
+
+def getTplStr(tplStr, data):
+    tps=splitStpl(tplStr)
+    preOpen=False
+    nextEnd=False
+    lenTps=len(tps)
+    for i in range(0,lenTps):
+        tStr=tps[i]
+        if i+1<lenTps:
+            nextStr=tps[i+1]
+        else:
+            nextStr=""
+        if nextStr=="}":
+            nextEnd=True
+        else:
+            nextEnd=False
+        if preOpen and nextEnd:
+            if tStr in data:
+                tps[i]=getStr(data[tStr])
+            else:
+                tps[i]="None"
+            tps[i-1]=""
+            tps[i+1]=""
+        if tStr=="{":
+            preOpen=True
+        else:
+            preOpen=False
+    return "".join(tps)
